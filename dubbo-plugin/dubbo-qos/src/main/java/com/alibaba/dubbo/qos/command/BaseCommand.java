69e3e0b28b829d8bd8671ce1b31bb5162aae0447
hebiao <hebiaoIT@163.com>
2018-09-11 14:06:05 +0800

    'tets'

--- dubbo-plugin/dubbo-qos/src/main/java/com/alibaba/dubbo/qos/command/BaseCommand.java
+++ dubbo-plugin/dubbo-qos/src/main/java/com/alibaba/dubbo/qos/command/BaseCommand.java
@@ -18,6 +18,7 @@ package com.alibaba.dubbo.qos.command;
 
 import com.alibaba.dubbo.common.extension.SPI;
 
+
 @SPI
 public interface BaseCommand {
     String execute(CommandContext commandContext,String[] args);
